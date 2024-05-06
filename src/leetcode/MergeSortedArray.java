529eb416f0826085c56c8361b3371d6dbbf67c7d
Hossam Rakha <hossamrakha0@gmail.com>
2024-05-06 20:23:00 +0300

    merge sorted array

---
+++ src/leetcode/MergeSortedArray.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeSortedArray {
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = new int[]{2, 5, 6};
+        int n = 3;
+
+        merge(nums1, m, nums2, n);
+
+    }
+
+    static public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int j = 0, i = m; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+    }
+}
