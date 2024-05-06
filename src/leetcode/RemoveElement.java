d033117575122beb97602327a4c3d9657ce608b5
Hossam Rakha <hossamrakha0@gmail.com>
2024-05-06 20:44:41 +0300

    remove element

---
+++ src/leetcode/RemoveElement.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class RemoveElement {
+
+
+    public static void main(String[] args) {
+       int[] nums = new int[] {3,2,2,3};
+       int val = 3;
+
+       int k =  removeElement(nums, val);
+       System.out.println(k);
+    }
+
+    static public int removeElement(int[] nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
